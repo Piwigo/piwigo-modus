bf721691f05f0c659ced26382f5d4123788cb001
Piwigo-TranslationTeam <translate-github@piwigo.org>
2014-05-15 07:41:10 +0000

    [modus] Update es_ES, thanks to : jpr928
    git-svn-id: http://piwigo.org/svn/extensions/modus@28483 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/es_ES/theme.lang.php
+++ language/es_ES/theme.lang.php
@@ -5,4 +5,11 @@ $lang['Explore'] = 'Explorar';
 $lang['%d album'] = '%d álbum';
 $lang['%d albums'] = '%d álbumes';
 
-?>
+$lang['%d pixels'] = '%d pixeles';
+$lang['Album thumbnails'] = 'Miniaturas de álbumes';
+$lang['Default size for thumbnails'] = 'Tamaño por defecto de las miniaturas';
+$lang['Default size for thumbnails on high density display (retina)'] = 'Tamaño por defecto de las miniaturas en pantallas de alta densidad (retina)';
+$lang['Default sizes'] = 'Tamaño por defecto';
+$lang['Modus theme config'] = 'Configuración del tema Modus';
+$lang['Skin'] = 'Estilo';
+$lang['Use square thumbs'] = 'Utilizar miniaturas cuadradas';
