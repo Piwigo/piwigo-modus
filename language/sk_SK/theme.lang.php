c44bd6334ea8ad10c32aed6f01efa2971d717d87
Piwigo-TranslationTeam <translate-github@piwigo.org>
2014-11-07 19:48:48 +0000

    [modus] Update sk_SK, thanks to dodo
    git-svn-id: http://piwigo.org/svn/extensions/modus@30388 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/sk_SK/theme.lang.php
+++ language/sk_SK/theme.lang.php
@@ -32,3 +32,4 @@ $lang['Default size for thumbnails on high density display (retina)'] = 'Predvol
 $lang['Default size for thumbnails'] = 'Predvolená veľkosť náhľadov';
 $lang['Album thumbnails'] = 'Náhľady albumu';
 $lang['%d pixels'] = '%d bodov';
+$lang['Display page banner'] = 'Zobraziť banner na stránku';
