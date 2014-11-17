912adc1923945c9631859096eec6b66958b05839
Piwigo-TranslationTeam <translate-github@piwigo.org>
2014-11-17 06:22:57 +0000

    [modus] Update el_GR, thanks to bas_alba
    git-svn-id: http://piwigo.org/svn/extensions/modus@30505 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/el_GR/theme.lang.php
+++ language/el_GR/theme.lang.php
@@ -32,3 +32,4 @@ $lang['Album thumbnails'] = 'Μικρογραφίες Λευκωμάτων ';
 $lang['%d pixels'] = '%d pixels';
 $lang['Use square thumbs'] = 'Χρήση τετράγωνων μικρογραφιών ';
 $lang['Skin'] = 'Skin';
+$lang['Display page banner'] = 'Προβολή Banner Σελίδας ';
