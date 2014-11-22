0a8af1c022c5e4e23f06f27d8330bec59bcb8ecc
Piwigo-TranslationTeam <Piwigo-TranslationTeam@users.noreply.github.com>
2014-11-22 09:26:48 +0100

    [modus] Update fr_FR, thanks to ddtddt

--- language/fr_FR/theme.lang.php
+++ language/fr_FR/theme.lang.php
@@ -3,7 +3,7 @@
 $lang['modus_theme'] = 'modus_theme';
 $lang['Explore'] = 'Explorer';
 $lang['%d album'] = '%d album';
-$lang['%d albums'] = '%d albums2';
+$lang['%d albums'] = '%d albums';
 
 $lang['Modus theme config'] = 'Configuration du thème Modus';
 $lang['Skin'] = 'Style';
