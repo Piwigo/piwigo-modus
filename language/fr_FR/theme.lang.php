2a9e4fb9de4b78949d8f28caaa8f8511b6aac746
Piwigo-TranslationTeam <Piwigo-TranslationTeam@users.noreply.github.com>
2014-11-22 09:26:31 +0100

    [modus] Update fr_FR, thanks to ddtddt

--- language/fr_FR/theme.lang.php
+++ language/fr_FR/theme.lang.php
@@ -3,7 +3,7 @@
 $lang['modus_theme'] = 'modus_theme';
 $lang['Explore'] = 'Explorer';
 $lang['%d album'] = '%d album';
-$lang['%d albums'] = '%d albums';
+$lang['%d albums'] = '%d albums2';
 
 $lang['Modus theme config'] = 'Configuration du thème Modus';
 $lang['Skin'] = 'Style';
@@ -14,4 +14,3 @@ $lang['Default sizes'] = 'Tailles par défaut';
 $lang['Default size for thumbnails'] = 'Taille par défaut pour les miniatures';
 $lang['Default size for thumbnails on high density display (retina)'] = 'Taille par défaut pour les miniatures sur écran haute densité (retina)';
 $lang['Display page banner'] = 'Afficher la bannière des pages';
-?>
