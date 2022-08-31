7ba99f352ec448cc3ffb9db0aa1fbf80cdc4b427
Matthieu Leproux <matthieu.leproux@gmail.com>
2022-08-31 14:26:27 +0200

    better svg support for modus, added condition to file-ext and path-ext classes for php8.1 compatibility

--- themeconf.inc.php
+++ themeconf.inc.php
@@ -208,10 +208,10 @@ function modus_thumbs($x, $smarty)
 		elseif ($csize[1] > $row_height)
 			$csize = $c->get_scaled_size(9999, $row_height);
 		if ($do_over) {?>
-<li style=width:<?=$csize[0]?>px;height:<?=$row_height?>px><a href="<?=$item['URL']?>"<?=$a_style?>><img src="<?=$c->get_url()?>" width=<?=$csize[0]?> height=<?=$csize[1]?> alt="<?=$item['TN_ALT']?>"></a><div class=overDesc><?=$item['NAME']?><?=$new?></div></li>
+<li class="path-ext-<?=$item["path_ext"]?> file-ext-<?=$item["file_ext"]?>" style=width:<?=$csize[0]?>px;height:<?=$row_height?>px><a href="<?=$item['URL']?>"<?=$a_style?>><img src="<?=$c->get_url()?>" width=<?=$csize[0]?> height=<?=$csize[1]?> alt="<?=$item['TN_ALT']?>"></a><div class=overDesc><?=$item['NAME']?><?=$new?></div></li>
 <?php
 		} else {?>
-<li style=width:<?=$csize[0]?>px;height:<?=$row_height?>px><a href="<?=$item['URL']?>"<?=$a_style?>><img src="<?=$c->get_url()?>" width=<?=$csize[0]?> height=<?=$csize[1]?> alt="<?=$item['TN_ALT']?>"></a></li>
+<li class="path-ext-<?=$item["path_ext"]?> file-ext-<?=$item["file_ext"]?>" style=width:<?=$csize[0]?>px;height:<?=$row_height?>px><a href="<?=$item['URL']?>"<?=$a_style?>><img src="<?=$c->get_url()?>" width=<?=$csize[0]?> height=<?=$csize[1]?> alt="<?=$item['TN_ALT']?>"></a></li>
 <?php
 		}
 	}
@@ -276,6 +276,9 @@ function modus_index_category_thumbnails($items)
 	{
 		$src_image = $item['representative']['src_image'];
 		$src_size = $src_image->get_size();
+    
+    $item['path_ext'] = strtolower(get_extension($item['representative']['path']));
+    $item['file_ext'] = strtolower(get_extension($item['representative']['file']));
 
 		$deriv = null;
 		if (isset($alt_params) && $src_size[0]>=$src_size[1])
